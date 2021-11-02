409daa7933fd5bca4fb19dbd6952a41ed6f05103
bugraaltun <80859368+bugraaltun@users.noreply.github.com>
2021-11-02 17:01:58 +0300

    Add files via upload

---
+++ diabetes_research_.py
@@ -0,0 +1,364 @@
+################################################
+# End-to-End Diabetes Machine Learning Pipeline I
+################################################
+
+# 1. Exploratory Data Analysis
+# 2. Data Preprocessing & Feature Engineering
+# 3. Base Models
+# 4. Automated Hyperparameter Optimization
+# 5. Stacking & Ensemble Learning
+# 6. Prediction for a New Observation
+# 7. Pipeline Main Function
+
+import joblib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+import missingno as msno
+from datetime import date
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, plot_roc_curve
+from sklearn.model_selection import train_test_split, cross_validate
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+def load_diabet():
+    data = pd.read_csv("Datasets/diabetes.csv")
+    return data
+
+
+################################################
+# 1. Exploratory Data Analysis
+################################################
+
+df = load_diabet()
+df.head()
+df.describe().T
+df.isnull().any()
+#No NA
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+# Parsing variable types
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+#Only outcome categoric
+
+# Checking of categorical variables
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+for col in cat_cols:
+    cat_summary(df, col)
+
+# Checking of numerical variables
+df[num_cols].describe().T
+
+# Correlation of numerical variables with each other
+correlation_matrix(df, num_cols)
+
+# Examination of numeric variables with Target
+for col in num_cols:
+    target_summary_with_num(df, "Outcome", col)
+
+################################################
+# 2. Data Preprocessing & Feature Engineering
+################################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+for i in df.columns:
+    print(i, check_outlier(df, i))
+
+df["Insulin"].describe()
+
+#There was only one outlier in the insulin column, and I don't touch it.
+
+
+#I DECIDED THE NUMBERS BY THE RESEARCH I DID.
+
+df["Glucose"].describe()
+
+df.loc[(df['Glucose'] <= 70), 'Glucose_Cat'] = 'Low'
+df.loc[(df['Glucose'] >= 71) & (df['Glucose'] < 125), 'Glucose_Cat'] = 'Normal'
+df.loc[(df['Glucose'] >= 125), 'Glucose_Cat'] = 'High'
+
+
+df["Insulin"].describe()
+
+df.loc[(df['Insulin'] < 110), 'Insulin_Cat'] = 'OK'
+df.loc[(df['Insulin'] >= 110) & (df['Insulin'] < 150), 'Insulin_Cat'] = 'Warning'
+df.loc[(df['Insulin'] >= 150), 'Insulin_Cat'] = 'High_Possibly_Diabet'
+
+
+df["BloodPressure"].describe()
+
+df.loc[(df['BloodPressure'] >= 67) & (df['BloodPressure'] <= 91), 'Blood_Cat'] = 'Normal'
+df.loc[(df['BloodPressure'] < 67), 'Blood_Cat'] = 'Low_Pressure'
+df.loc[(df['BloodPressure'] > 91), 'Blood_Cat'] = 'High_Pressure'
+
+
+df["Age"].describe()
+
+df.loc[(df['Age'] <= 22), 'New_Age_Cat'] = 'Young'
+df.loc[(df['Age'] >= 23) & (df['Age'] < 56), 'New_Age_Cat'] = 'Mature'
+df.loc[(df['Age'] >= 56), 'New_Age_Cat'] = 'Senior'
+
+df.tail()
+
+high_correlated_cols(df, plot=True)
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+for col in cat_cols:
+    target_summary_with_cat(df, "Outcome", col)
+
+cat_cols = [col for col in cat_cols if "Outcome" not in col]
+
+#######################
+# ENCODING
+#######################
+
+#Label Encoding
+binary_cols = [col for col in df.columns if df[col].dtype not in [int, float]
+               and df[col].nunique() == 2]
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+df.head()
+# ONE HOT ENCODING
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+df = one_hot_encoder(df, ohe_cols)
+
+df["Insulin"].value_counts()
+cols = [col for col in df.columns if "Outcome" not in col]
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+for col in cols:
+    target_summary_with_num(df, "Outcome", col)
+
+# Standartization
+
+scaler = RobustScaler()
+
+df[cols] = scaler.fit_transform(df[cols])
+df.head()
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+######################################################
+# 3. Base Models
+######################################################
+
+# Selection of dependent and independent variables:
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)
+
+def base_models(X, y, scoring="roc_auc"):
+    print("Base Models....")
+    classifiers = [('LR', LogisticRegression()),
+                   ('KNN', KNeighborsClassifier()),
+                   ("SVC", SVC()),
+                   ("CART", DecisionTreeClassifier()),
+                   ("RF", RandomForestClassifier()),
+                   ('Adaboost', AdaBoostClassifier()),
+                   ('GBM', GradientBoostingClassifier()),
+                   ('XGBoost', XGBClassifier(use_label_encoder=False, eval_metric='logloss')),
+                   ('LightGBM', LGBMClassifier()),
+                   # ('CatBoost', CatBoostClassifier(verbose=False))
+                   ]
+
+    for name, classifier in classifiers:
+        cv_results = cross_validate(classifier, X, y, cv=3, scoring=scoring)
+        print(f"{scoring}: {round(cv_results['test_score'].mean(), 4)} ({name}) ")
+
+base_models(X, y, scoring="f1")
+
+
+######################################################
+# 4. Automated Hyperparameter Optimization
+######################################################
+
+knn_params = {"n_neighbors": range(2, 50)}
+
+cart_params = {'max_depth': range(1, 20),
+               "min_samples_split": range(2, 30)}
+
+rf_params = {"max_depth": [8, 15, None],
+             "max_features": [5, 7, "auto"],
+             "min_samples_split": [15, 20],
+             "n_estimators": [200, 300]}
+
+xgboost_params = {"learning_rate": [0.1, 0.01],
+                  "max_depth": [5, 8],
+                  "n_estimators": [100, 200],
+                  "colsample_bytree": [0.5, 1]}
+
+lightgbm_params = {"learning_rate": [0.01, 0.1],
+                   "n_estimators": [300, 500],
+                   "colsample_bytree": [0.7, 1]}
+
+classifiers = [('KNN', KNeighborsClassifier(), knn_params),
+               ("CART", DecisionTreeClassifier(), cart_params),
+               ("RF", RandomForestClassifier(), rf_params),
+               ('XGBoost', XGBClassifier(use_label_encoder=False, eval_metric='logloss'), xgboost_params),
+               ('LightGBM', LGBMClassifier(), lightgbm_params)]
+
+
+def hyperparameter_optimization(X, y, cv=3, scoring="roc_auc"):
+    print("Hyperparameter Optimization....")
+    best_models = {}
+    for name, classifier, params in classifiers:
+        print(f"########## {name} ##########")
+        cv_results = cross_validate(classifier, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (Before): {round(cv_results['test_score'].mean(), 4)}")
+
+        gs_best = GridSearchCV(classifier, params, cv=cv, n_jobs=-1, verbose=False).fit(X, y)
+        final_model = classifier.set_params(**gs_best.best_params_)
+
+        cv_results = cross_validate(final_model, X, y, cv=cv, scoring=scoring)
+        print(f"{scoring} (After): {round(cv_results['test_score'].mean(), 4)}")
+        print(f"{name} best params: {gs_best.best_params_}", end="\n\n")
+        best_models[name] = final_model
+    return best_models
+
+
+best_models = hyperparameter_optimization(X, y)
+
+######################################################
+# 5. Stacking & Ensemble Learning
+######################################################
+
+def voting_classifier(best_models, X, y):
+    print("Voting Classifier...")
+    voting_clf = VotingClassifier(estimators=[('KNN', best_models["KNN"]), ('RF', best_models["RF"]),
+                                              ('LightGBM', best_models["LightGBM"])],
+                                  voting='soft').fit(X, y)
+    cv_results = cross_validate(voting_clf, X, y, cv=3, scoring=["accuracy", "f1", "roc_auc"])
+    print(f"Accuracy: {cv_results['test_accuracy'].mean()}")
+    print(f"F1Score: {cv_results['test_f1'].mean()}")
+    print(f"ROC_AUC: {cv_results['test_roc_auc'].mean()}")
+    return voting_clf
+
+
+voting_clf = voting_classifier(best_models, X, y)
+
+######################################################
+# 6. Prediction for a New Observation
+######################################################
+
+X.columns
+random_user = X.sample(1, random_state=45)
+voting_clf.predict(random_user)
